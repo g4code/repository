76184cbf281c3c117d73f4271aa2efd608396101
Danijel Bjelancevic <danijel.bjelancevic@gmail.com>
2018-02-14 13:42:25 +0100

    000 - Fixed a Cache class according to PSR-2 standards.

--- src/Adapters/Cache.php
+++ src/Adapters/Cache.php
@@ -9,21 +9,39 @@ use G4\Repository\RepositoryIdentity;
 
 class Cache implements AdapterInterface
 {
+    /** @var \G4\Mcache\Mcache */
     private $mcache;
+
+    /** @var RepositoryIdentity */
     private $identity;
+
+    /** @var int */
     private $lifeTime;
 
-    public function __construct(\G4\Mcache\Mcache $mcacheInstance, RepositoryIdentity $identity, $lifeTime = CacheLifetime::LIFETIME_S)
-    {
+    /**
+     * Cache constructor.
+     * @param \G4\Mcache\Mcache $mcacheInstance
+     * @param RepositoryIdentity $identity
+     * @param int $lifeTime
+     */
+    public function __construct(
+        \G4\Mcache\Mcache $mcacheInstance,
+        RepositoryIdentity $identity,
+        $lifeTime = CacheLifetime::LIFETIME_S
+    ) {
         $this->mcache = $mcacheInstance;
         // should be generated with domain, service name and params
         $this->identity      = $identity;
         $this->lifeTime = $lifeTime;
     }
 
+    /**
+     * @return bool
+     */
     public function has()
     {
         $response = $this->get();
+
         return !empty($response);
     }
 
@@ -36,6 +54,9 @@ class Cache implements AdapterInterface
             ->set();
     }
 
+    /**
+     * @return mixed
+     */
     public function get()
     {
         return  $this->mcache
@@ -43,26 +64,39 @@ class Cache implements AdapterInterface
             ->get();
     }
 
+    /**
+     * @return string
+     */
     public function getPriority()
     {
         return PersistentPriority::MEDIUM;
     }
 
+    /**
+     * @param array $data
+     * @return $this
+     */
     public function post($data = [])
     {
-        if(!empty($data)){
+        if (!empty($data)) {
             $this->mcache
                 ->key($this->identity->getCacheKey())
                 ->value($data)
                 ->expiration($this->lifeTime)
                 ->set();
         }
+
         return $this;
     }
 
+    /**
+     * @param array $data
+     * @return $this
+     */
     public function put($data = [])
     {
         $this->post($data);
+
         return $this;
     }
 
